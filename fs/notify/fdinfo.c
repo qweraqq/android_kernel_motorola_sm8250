fdfde8b51f8fd5e22134cc4ea79a99250ce304cf
shenxx <shenxiangxiang@gmail.com>
2025-10-17 14:14:18 +0800

    Upstream commit a32e697cda27679a0327ae2cafdad8c7170f548f
    The inotify mask flags IN_ONESHOT and IN_EXCL_UNLINK are not "internal
    to kernel" and should be exposed in procfs fdinfo so CRIU can restore
    them.
    
    Fixes: 6933599697c9 ("inotify: hide internal kernel bits from fdinfo")
    Link: https://lore.kernel.org/r/20220422120327.3459282-2-amir73il@gmail.com
    Signed-off-by: Amir Goldstein amir73il@gmail.com
    Signed-off-by: Jan Kara jack@suse.cz
    Signed-off-by: Sasha Levin sashal@kernel.org
    ---
     fs/notify/fdinfo.c               | 11 ++---------
     fs/notify/inotify/inotify.h      | 12 ++++++++++++
     fs/notify/inotify/inotify_user.c |  2 +-
     3 files changed, 15 insertions(+), 10 deletions(-)
    
    diff --git a/fs/notify/fdinfo.c b/fs/notify/fdinfo.c
    index 86fcf5814279..74aeabbf0ea4 100644
    --- a/fs/notify/fdinfo.c
    +++ b/fs/notify/fdinfo.c
    @@ -83,16 +83,9 @@ static void inotify_fdinfo(struct seq_file *m, struct fsnotify_mark *mark)
        inode_mark = container_of(mark, struct inotify_inode_mark, fsn_mark);
        inode = igrab(fsnotify_conn_inode(mark->connector));
        if (inode) {
    -		/*
    -		 * IN_ALL_EVENTS represents all of the mask bits
    -		 * that we expose to userspace.  There is at
    -		 * least one bit (FS_EVENT_ON_CHILD) which is
    -		 * used only internally to the kernel.
    -		 */
    -		u32 mask = mark->mask & IN_ALL_EVENTS;
    -		seq_printf(m, "inotify wd:%x ino:%lx sdev:%x mask:%x ignored_mask:%x ",
    +		seq_printf(m, "inotify wd:%x ino:%lx sdev:%x mask:%x ignored_mask:0 ",
        		   inode_mark->wd, inode->i_ino, inode->i_sb->s_dev,
    -			   mask, mark->ignored_mask);
    +			   inotify_mark_user_mask(mark));
        	show_mark_fhandle(m, inode);
        	seq_putc(m, '\n');
        	iput(inode);
    diff --git a/fs/notify/inotify/inotify.h b/fs/notify/inotify/inotify.h
    index 7e4578d35b61..5d94c00b1233 100644
    --- a/fs/notify/inotify/inotify.h
    +++ b/fs/notify/inotify/inotify.h
    @@ -21,6 +21,18 @@ static inline struct inotify_event_info *INOTIFY_E(struct fsnotify_event *fse)
        return container_of(fse, struct inotify_event_info, fse);
     }
    
    +/*
    + * INOTIFY_USER_FLAGS represents all of the mask bits that we expose to
    + * userspace.  There is at least one bit (FS_EVENT_ON_CHILD) which is
    + * used only internally to the kernel.
    + */
    +#define INOTIFY_USER_MASK (IN_ALL_EVENTS | IN_ONESHOT | IN_EXCL_UNLINK)
    +
    +static inline __u32 inotify_mark_user_mask(struct fsnotify_mark *fsn_mark)
    +{
    +	return fsn_mark->mask & INOTIFY_USER_MASK;
    +}
    +
     extern void inotify_ignored_and_remove_idr(struct fsnotify_mark *fsn_mark,
        				   struct fsnotify_group *group);
     extern int inotify_handle_event(struct fsnotify_group *group,
    diff --git a/fs/notify/inotify/inotify_user.c b/fs/notify/inotify/inotify_user.c
    index 97a51690338e..83d0b9356844 100644
    --- a/fs/notify/inotify/inotify_user.c
    +++ b/fs/notify/inotify/inotify_user.c
    @@ -96,7 +96,7 @@ static inline __u32 inotify_arg_to_mask(u32 arg)
        mask = (FS_IN_IGNORED | FS_EVENT_ON_CHILD | FS_UNMOUNT);
    
    /* mask off the flags used to open the fd */
    -	mask |= (arg & (IN_ALL_EVENTS | IN_ONESHOT | IN_EXCL_UNLINK));
    +	mask |= (arg & INOTIFY_USER_MASK);
    
    return mask;
     }

--- fs/notify/fdinfo.c
+++ fs/notify/fdinfo.c
@@ -90,9 +90,9 @@ static void inotify_fdinfo(struct seq_file *m, struct fsnotify_mark *mark)
 		 * used only internally to the kernel.
 		 */
 		u32 mask = mark->mask & IN_ALL_EVENTS;
-		seq_printf(m, "inotify wd:%x ino:%lx sdev:%x mask:%x ignored_mask:%x ",
-			   inode_mark->wd, inode->i_ino, inode->i_sb->s_dev,
-			   mask, mark->ignored_mask);
+		seq_printf(m, "inotify wd:%x ino:%lx sdev:%x mask:%x ignored_mask:0 ",
+			inode_mark->wd, inode->i_ino, inode->i_sb->s_dev,
+			inotify_mark_user_mask(mark));
 		show_mark_fhandle(m, inode);
 		seq_putc(m, '\n');
 		iput(inode);
